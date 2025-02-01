ead47c06f6fc524abea52d687de0adc14fa143a8
Andrey Platov <andrey@hulylabs.com>
2025-02-01 15:46:01 +0100

    checkpoint
    Signed-off-by: Andrey Platov <andrey@hulylabs.com>

--- rebel/src/ops.rs
+++ rebel/src/ops.rs
@@ -2,17 +2,48 @@
 
 use super::{Offset, Word};
 
-pub trait Heap: AsRef<[Word]> {
-    fn get_block(&self, addr: Offset) -> Option<&[Word]> {
+struct Block<T>(T);
+
+impl<T> Block<T>
+where
+    T: AsRef<[Word]>,
+{
+    fn get_block(&self, addr: Offset) -> Option<Block<&[Word]>> {
+        let data = self.0.as_ref();
         let addr = addr as usize;
-        let len = self.as_ref().get(addr).copied()? as usize;
-        self.as_ref().get(addr + 1..addr + 1 + len)
+        let len = data.get(addr).copied()? as usize;
+        let start = addr + 1;
+        data.get(start..start + len).map(|block| Block(block))
     }
 }
 
-pub trait Alloc: AsMut<[Word]> {
+impl<T> Block<T>
+where
+    T: AsMut<[Word]>,
+{
+    fn init(&mut self, size: u32) -> Option<()> {
+        self.0.as_mut().first_mut().map(|slot| *slot = size)
+    }
+
+    fn poke<const N: usize>(&mut self, addr: Offset, words: [u32; N]) -> Option<()> {
+        let (len, data) = self.0.as_mut().split_first_mut()?;
+        let addr = addr as usize;
+        if addr + N > *len as usize {
+            None
+        } else {
+            data.get_mut(addr..addr + N).map(|block| {
+                block
+                    .iter_mut()
+                    .zip(words.iter())
+                    .for_each(|(slot, value)| {
+                        *slot = *value;
+                    });
+            })
+        }
+    }
+
     fn alloc<const N: usize>(&mut self, words: [u32; N]) -> Option<Offset> {
-        let (len, data) = self.as_mut().split_first_mut()?;
+        let (len, data) = self.0.as_mut().split_first_mut()?;
         let addr = *len as usize;
         data.get_mut(addr..addr + N).map(|block| {
             block
@@ -27,7 +58,7 @@ pub trait Alloc: AsMut<[Word]> {
     }
 
     fn alloc_n(&mut self, words: u32) -> Option<(Offset, &mut [Word])> {
-        let (len, data) = self.as_mut().split_first_mut()?;
+        let (len, data) = self.0.as_mut().split_first_mut()?;
         data.get_mut(*len as usize..(*len + words) as usize)
             .map(|data| (*len, data))
             .inspect(|_| *len += words)
@@ -44,22 +75,39 @@ pub trait Alloc: AsMut<[Word]> {
 
     fn get_block_mut(&mut self, addr: Offset) -> Option<&mut [Word]> {
         let addr = addr as usize;
-        let len = self.as_mut().get(addr).copied()? as usize;
-        self.as_mut().get_mut(addr + 1..addr + 1 + len)
+        let len = self.0.as_mut().get(addr).copied()? as usize;
+        self.0.as_mut().get_mut(addr + 1..addr + 1 + len)
     }
 }
 
-pub trait StackMut: Alloc {
+//
+
+pub struct Stack<T>(T);
+
+impl<T> Stack<T>
+where
+    T: AsMut<[Word]>,
+{
     fn init(&mut self) -> Option<()> {
-        self.as_mut().first_mut().map(|slot| *slot = 0)
+        self.0.as_mut().first_mut().map(|slot| *slot = 0)
     }
 
     fn push<const N: usize>(&mut self, words: [u32; N]) -> Option<()> {
-        self.alloc(words).map(|_| ())
+        let (len, data) = self.0.as_mut().split_first_mut()?;
+        let addr = *len as usize;
+        data.get_mut(addr..addr + N).map(|block| {
+            block
+                .iter_mut()
+                .zip(words.iter())
+                .for_each(|(slot, value)| {
+                    *slot = *value;
+                });
+            *len += N as u32
+        })
     }
 
     fn pop<const N: usize>(&mut self) -> Option<[u32; N]> {
-        let (len, data) = self.as_mut().split_first_mut()?;
+        let (len, data) = self.0.as_mut().split_first_mut()?;
         len.checked_sub(N as u32).and_then(|new_len| {
             let addr = new_len as usize;
             data.get(addr..addr + N).map(|block| {
@@ -79,33 +127,37 @@ pub trait StackMut: Alloc {
 
 //
 
-impl Alloc for &mut [Word] {}
-impl StackMut for &mut [Word] {}
-
-pub struct Memory<'a, T> {
-    data: T,
-    stack: &'a mut [Word],
+pub struct MemoryLayout {
+    stack: Offset,
+    ops: Offset,
 }
 
-impl<'a, T> Memory<'a, T>
+impl MemoryLayout {
+    const LAYOUT_SIZE: u32 = 2;
+}
+
+pub struct Memory<T>(Block<T>);
+
+impl<T> Memory<T>
 where
-    T: StackMut,
+    T: AsMut<[Word]>,
 {
-    pub fn init(&'a mut self) -> Option<()> {
-        self.data.alloc_block(10).map(|(addr, mut stack)| {
-            stack.init();
-            self.stack = stack;
-        })
-    }
+    fn init(memory: T, stack_size: Offset, ops_size: Offset) -> Option<MemoryLayout> {
+        let mut heap = Block(memory);
+        heap.init(MemoryLayout::LAYOUT_SIZE)?;
 
-    pub fn alloc_stack(&mut self, words: u32) -> Option<Offset> {
-        self.data.alloc_block(words).map(|(addr, mut stack)| {
-            stack.init();
-            addr
-        })
+        let stack = heap
+            .alloc_block(stack_size)
+            .and_then(|(addr, data)| Stack(data).init().map(|_| addr))?;
+        let ops = heap
+            .alloc_block(ops_size)
+            .and_then(|(addr, data)| Stack(data).init().map(|_| addr))?;
+
+        heap.poke(0, [stack, ops])?;
+        Some(MemoryLayout { stack, ops })
     }
 }
 
-pub fn test(memory: &mut Memory<&mut [Word]>) -> Option<Offset> {
-    memory.alloc_stack(10)
+pub fn init(memory: &mut [Word]) -> Option<MemoryLayout> {
+    Memory::init(memory, 256, 256)
 }
